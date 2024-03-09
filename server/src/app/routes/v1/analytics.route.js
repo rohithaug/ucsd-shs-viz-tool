0e8e759ff01171eb09d176b710f9161407748051
vishaln15 <nagarajanvishal@gmail.com>
2024-03-08 23:42:09 -0800

    [ADD] Metrics API support for individual and bulk blogs

--- server/src/app/routes/v1/analytics.route.js
+++ server/src/app/routes/v1/analytics.route.js
@@ -8,4 +8,10 @@ const { analyticsController } = require("../../controllers");
 // GET REQUEST FOR ANALYTICS DATA
 router.get("/metrics", analyticsController.getMetrics);
 
+// GET REQUEST FOR ANALYTICS DATA FOR BULK BLOGS
+router.get("/metrics/bulk", analyticsController.getBulkMetrics);
+
+// GET REQUEST FOR ANALYTICS DATA FOR A SPECIFIC BLOG
+router.get("/metrics/:blogId", analyticsController.getBlogMetrics);
+
 module.exports = router;
